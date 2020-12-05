02c87ceb73748a3bf1f7d0706ccbc9506c337ba3
Thomas Bury <tombury182@gmail.com>
2020-12-05 11:42:42 -0500

    make grid plot to show contact rate

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ import numpy as np
 import pandas as pd
 
 import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+
 
 import dash
 import dash_core_components as dcc
@@ -41,12 +45,145 @@ server = app.server
 
 
 # Import covid data
-df_covid = pd.read_csv('data/df_covid.csv')
+df_covid = pd.read_csv('data/df_covid2.csv')
 list_countries = df_covid['location'].unique()
 
 #------------
 # functions
 #-------------
+
+
+
+
+def make_grid_plot(countries, res):
+    '''
+    Make grid plot showing covid trajectories and
+    derived metrics.
+
+    Parameters
+    ----------
+    countries : list of strings
+        List of countries to plot
+    res : string
+        One of ['Daily','7 day average']
+
+    Returns
+    -------
+    Plotly fig.
+
+    ''' 
+    
+
+    fig = make_subplots(rows=4, cols=1,
+                        shared_xaxes=True,
+                        vertical_spacing=0.02   ,
+                        )
+    
+    cols = [
+        '#1f77b4',  # muted blue
+        '#ff7f0e',  # safety orange
+        '#2ca02c',  # cooked asparagus green
+        '#d62728',  # brick red
+        '#9467bd',  # muted purple
+        '#8c564b',  # chestnut brown
+        '#e377c2',  # raspberry yogurt pink
+        '#7f7f7f',  # middle gray
+        '#bcbd22',  # curry yellow-green
+        '#17becf'   # blue-teal
+    ]    
+    
+    
+    if res=='Daily':
+        cases_col = 'new_cases'
+        deaths_col = 'new_deaths'
+        I_col = 'I'
+        R_col = 'R'
+    else:
+        cases_col = 'new_cases_7dayAv'
+        deaths_col = 'new_deaths_7dayAv'
+        I_col = 'I'
+        R_col = 'R_7dayAv'
+
+
+    
+    # Loop through each country
+    count=0
+    for country in countries:
+        df_plot = df_covid[df_covid['location']==country]
+        
+        # Plot new cases
+        fig.add_trace(
+            go.Scatter(x=df_plot['date'],
+                       y=df_plot[cases_col],
+                       mode='lines',
+                       name=country,
+                       legendgroup=country,
+                       marker={'color':cols[count]},
+                       ),
+            row=1,col=1,
+        )
+        
+        # Plot new deaths
+        fig.add_trace(
+            go.Scatter(x=df_plot['date'],
+                       y=df_plot[deaths_col],
+                       mode='lines',
+                       name=country,
+                       legendgroup=country,
+                       showlegend=False,
+                       marker={'color':cols[count]},
+                       ),
+            row=2,col=1,
+        )    
+        
+        # Plot total infected
+        fig.add_trace(
+            go.Scatter(x=df_plot['date'],
+                       y=df_plot[I_col],
+                       mode='lines',
+                       name=country,
+                       legendgroup=country,
+                       showlegend=False,
+                       marker={'color':cols[count]},
+                       ),
+            row=3,col=1,
+        )           
+        
+        # Plot contact ratio
+        fig.add_trace(
+            go.Scatter(x=df_plot['date'],
+                       y=df_plot[R_col],
+                       mode='lines',
+                       name=country,
+                       legendgroup=country,
+                       showlegend=False,
+                       marker={'color':cols[count]},
+                       ),
+            row=4,col=1,
+        )
+        
+        count+=1
+    
+    # Layout properties
+    fig.update_xaxes(title='Date',row=4,col=1)
+    fig.update_yaxes(title='New cases',row=1,col=1)
+    fig.update_yaxes(title='New deaths',row=2,col=1)
+    fig.update_yaxes(title='Infected',row=3,col=1)
+    fig.update_yaxes(title='Contact ratio',row=4,col=1)
+    
+    fig.update_layout(height=700,
+                      margin={'l':0,'r':0,'b':20,'t':40}
+                      )
+    
+    
+    return fig
+
+
+
+fig = make_grid_plot(['United States','United Kingdom'],'Daily')
+fig.write_html('temp.html')
+
+
 
 
 def make_traj_plot(countries, var, res):
@@ -98,10 +235,52 @@ def make_traj_plot(countries, var, res):
     
     fig.update_xaxes(title='Date')
     fig.update_yaxes(title=var)
-    fig.update_layout(legend_title='Country')
+    fig.update_layout(legend_title='Country',
+                      height=300)
 
     return fig
     
+
+def make_r_plot(countries):
+    '''
+    Make plot of disease contact rate (R)
+
+    Parameters
+    ----------
+    countries : list of strings
+        List of countries to plot
+
+    Returns
+    -------
+    Plotly fig.
+
+    '''
+
+
+   
+    # Filter df to only include list of countries provided
+    df_plot = df_covid[df_covid['location'].isin(countries)]
+
+    # If df_plot is empty, make empty plot
+    if len(df_plot)==0:
+        fig = px.line(df_plot,
+                      x='date',
+                      y='R')
+
+    else:
+        fig = px.line(df_plot,
+                      x='date',
+                      y='R',
+                      color='location')
+    
+    fig.update_xaxes(title='Date')
+    fig.update_yaxes(title='R')
+    fig.update_layout(legend_title='Country',
+                      height=300)
+
+    return fig
+    
+
 
 
 #-----------------
@@ -111,12 +290,11 @@ def make_traj_plot(countries, var, res):
 
 # Defualt values for simulation
 def_countries = ['United States']
-def_var = 'New cases'
 def_res = 'Daily'
 
 
-# Make line plot of covid trajectories
-fig_traj = make_traj_plot(def_countries, def_var, def_res)
+# Make grid plot
+fig_grid = make_grid_plot(def_countries, def_res)
 
 
 #--------------------
@@ -161,19 +339,7 @@ app.layout = html.Div([
         
         html.Br(),
       
-        # Dropdown box for variable to plot
-        html.Label('Variable',
-                   style={'fontSize':14},
-        ),         
-        dcc.Dropdown(
-            id='dropdown_var',
-            options=opts_var,
-            value=def_var,
-            optionHeight=20,
-            clearable=False,
-        ),
-         
-        html.Br(),
+
 
 
         # Dropdown box for temporal resolution
@@ -191,8 +357,8 @@ app.layout = html.Div([
         
         
         ],       
-		style={'width':'30%',
-			   'height':'500px',
+		style={'width':'25%',
+			   'height':'1000px',
 			   'fontSize':'10px',
 			   'padding-left':'5%',
 			   'padding-right':'5%',
@@ -205,19 +371,21 @@ app.layout = html.Div([
 
  	# Trajectory figure
  	html.Div(
-		[dcc.Graph(id='fig_traj',
- 				   figure = fig_traj,
+		[dcc.Graph(id='fig_grid',
+ 				   figure = fig_grid,
  				   # config={'displayModeBar': False},
  				   ),
  		 ],
-		style={'width':'55%',
-			   'height':'500px',
+		style={'width':'60%',
+			   'height':'1000px',
 			   'fontSize':'10px',
 			   'padding-left':'0%',
 			   'padding-right':'5%',
 			   'vertical-align': 'middle',
 			   'display':'inline-block'},
  	),
+
+
 
 ])
 
@@ -229,26 +397,27 @@ app.layout = html.Div([
 
 # Update figures
 @app.callback(
-            Output('fig_traj','figure'),
+            
+            Output('fig_grid','figure'),
+            
             [
               Input('dropdown_countries','value'),
-              Input('dropdown_var','value'),    
               Input('dropdown_res','value'),           
             ],
             )
 
-def update_figs(countries, var, res):
+def update_figs(countries, res):
     
     # Make figure of trajectories
-    fig_traj = make_traj_plot(countries, var, res)
+    fig_grid = make_grid_plot(countries, res)
 
-    return fig_traj
+    return fig_grid
 
 
 
-#-----------------
-# Add the server clause
-#–-----------------
+# -----------------
+# # Add the server clause
+# #–-----------------
 
 if __name__ == '__main__':
     app.run_server(
